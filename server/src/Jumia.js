450c843d8c36a99b9188e4d3e0c0ee4af02d0b73
Karim Salem <karimsalem25@gmail.com>
2022-01-10 19:44:40 +0200

    added documentation

--- server/src/Jumia.js
+++ server/src/Jumia.js
@@ -1,9 +1,35 @@
+/* Puppeteer is a Node library that we can use to control a headless Chrome instance. 
+    We are basically using Chrome,
+    but programmatically using JavaScript. 
+    
+   By pupteer we can:
+    scrape web pages
+    automate form submissions
+    perform any kind of browser automation
+    create server-side rendered versions of single page apps
+    make screenshots
+    create automating testing 
+    */
+
 const puppeteer = require('puppeteer')
 const screenshot = 'jumia.png'
 const Product = require('./schema')
+/* 
+    An async function is a function declared with the async keyword,
+     and the await keyword is permitted within them. 
+    The async and await keywords enable asynchronous,
+     promise-based behavior to be written in a cleaner style,
+     avoiding the need to explicitly configure promise chains
 
+*/
 async function getjumiaItems(itemsname) {
-
+/*
+    the launch method initializes the instance at first, 
+    and then attaching Puppeteer to that. 
+    Notice this method is asynchronous  which,
+     , returns a Promise. Once it’s resolved, 
+    we get a browser instance that represents our initialized instance.
+    */
     const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
     await page.setViewport({
@@ -11,8 +37,9 @@ async function getjumiaItems(itemsname) {
         height: 1080
     })
     await page.goto('https://www.jumia.com/', { waitUntil: 'networkidle2' })
-    await page.type('#fi-q', itemsname)
-    await page.keyboard.press('Enter');
+     //networkidle2 - consider navigation to be finished when there are no more than 2 network connections for at least 500 ms
+    await page.type('#fi-q', itemsname)//name of search box in jumia
+    await page.keyboard.press('Enter');//clicking enter there was no submit button in jumia 
     await page.waitForSelector('.img-c')
     await page.waitForTimeout(2000)
     await page.screenshot({ path: screenshot })
@@ -20,6 +47,9 @@ async function getjumiaItems(itemsname) {
 
     const grabprices = await page.evaluate(() => {
 
+         /*querySelectorAll() The Document method querySelectorAll() returns a static NodeList 
+                representing a list of the document's elements that match the specified group of selectors.
+        */
 
 
         const nametag = document.querySelectorAll('.name')
@@ -27,6 +57,9 @@ async function getjumiaItems(itemsname) {
         const pricetag = document.querySelectorAll('.prc')
 
         let prices = []
+         /* 
+        The forEach() method calls a function for each element in an array
+        */
         pricetag.forEach((price) => {
             prices.push(price.innerText.replace(/[^\d]/g, ''));
         })
@@ -34,7 +67,14 @@ async function getjumiaItems(itemsname) {
         nametag.forEach((name) => {
             names.push(name.innerText);
         })
+         // we pushed names and prices of products in an array
         let products = [];
+         
+        
+        
+        /*
+            for loop that saves products names and price
+        */
 
         for (let i = 0; i < names.length; i++) {
             products.push({ Name: names[i], Price: prices[i] });
